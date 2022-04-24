7d203b7c09f3f37838c2d91301a9d828e2c72461
franc <franc@MSI>
2022-04-23 20:23:10 -0600

    1era Practica de Lab Programacion 1

---
+++ src/practica/labp1/PracticaLabP1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.labp1;
+
+/**
+ *
+ * @author franc
+ */
+public class PracticaLabP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        System.out.println("-Valor Numerico:"+(10+12.34)+"$ \nEstado: "+true+"- Habilitado");
+        System.out.print("Datos de alumnos\nCuenta: 22211179\nNombre Alumno: Jorge Francisco\nCarrera: Ingenieria Sistemas\n");
+    }
+    
+}
